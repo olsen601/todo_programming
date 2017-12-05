beafb9d6e6ed0c372afc00d58e2a7086cacea68f
olsen601 <tedssmith2556@gmail.com>
2017-12-05 00:56:47 -0600

    add flash require back into app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var flash = require('express-flash');
 var session = require('express-session');
 var mongoose = require('mongoose');
 var MongoDBStore = require('connect-mongodb-session')(session);
