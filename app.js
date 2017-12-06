fdd090c4a735822757145b175ac4c4e474ff4120
olsen601 <tedssmith2556@gmail.com>
2017-12-06 13:17:00 -0600

    altered app.js to contain app.use for tasks.js at project/_id

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/auth', auth);
 app.use('/', projects);
-app.use('/project', tasks);
+app.use('/project/'+req.user._id, tasks);
 
 
 // catch 404 and forward to error handler
