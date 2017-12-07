56e2eb64d22a19ea5f571b834d2d1b2335c51a66
olsen601 <tedssmith2556@gmail.com>
2017-12-07 15:44:47 -0600

    alter hbs task route again

--- routes/tasks.js
+++ routes/tasks.js
@@ -39,7 +39,7 @@ So the req.params._id will be the ObjectId of the task to find
       if (!task) {
         res.status(404).send('Task not found');
       }
-      else if ( req.params._id.equals(task._id)) {
+      else if ( req.params._id.equals(req.task._id)) {
         // Does this task belong to this user?
         res.render('task', {title: 'Task', task: task});
       }
